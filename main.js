b1837f987863f68eaefd24811e57848c72a14263
Saanvi-21 <71885805+Saanvi-21@users.noreply.github.com>
2020-10-08 18:45:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload(){
+    nose = loadImage("https://i.postimg.cc/8CrDpxLd/nose.png");
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialised");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+    console.log(results);
+    nose_x = results[0].pose.nose.x;
+    nose_y = results[0].pose.nose.y
+    console.log("nose x - "+ nose_x);
+    console.log("nose y - "+ nose_y);
+    }
+}
+
+
+function draw(){
+   image(video,0,0,300,300);
+   image(nose,nose_x-30,nose_y-10,50,50)
+}
+
+function take_snapshot(){
+    save("Your Filtered Image");
+}
